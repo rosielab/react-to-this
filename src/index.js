8c7f5e205597e816fecaeb7f1794bec8dfb8084b
laurenyip <laurenyip10@gmail.com>
2024-03-09 16:52:30 -0800

    tracking

--- src/index.js
+++ src/index.js
@@ -6,11 +6,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
